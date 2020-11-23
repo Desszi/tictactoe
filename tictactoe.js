69f9f71c4d5d5ef81ee3ef22f94f3ab2f4d4a304
Mercédesz Anett Porkoláb <09dporkolabm@edu.bme.hu>
2020-11-23 22:00:47 +0100

    Kész kb.

--- tictactoe.js
+++ tictactoe.js
@@ -1,74 +1,92 @@
 'use strict'
 
-const dataTictactoe = Array.from(document.querySelectorAll('.ground td[data-value]'));
-
+let signal = "O";
 const TicArray = [[""," "," "],[" ", " ", " "],[" ", " ", " "]];
-let k=0;
-for (let i =0; i< 3; i+=1) {
-    for(let j=0; j<3; j+=1) {
-        TicArray[i][j] = dataTictactoe[k];
-        k++;
+
+function myFunction() { 
+    const dataTictactoe = Array.from(document.querySelectorAll('.ground td[data-value]'));
+    
+    let k=0;
+    for (let i =0; i< 3; i+=1) {
+        for(let j=0; j<3; j+=1) {
+            TicArray[i][j] = dataTictactoe[k];
+            k++;
+        }
+    }
+
+       for (let i = 0; i < TicArray.length; i++) {
+            for (let j = 0; j < TicArray[i].length; j++) {
+                TicArray[i][j].onclick = function () { 
+                        TicArray[i][j].innerHTML = signal;
+                        Rule();
+                        if (signal === "O") {
+                            signal = "X";
+                        } 
+                        else if (signal==="X") {
+                            signal = "O";
+                        }
+                };
+            }
+        }
+};
+
+
+function Rule() {
+    if (TicArray[0][0].innerHTML === 'X' && TicArray[0][1].innerHTML==='X' && TicArray[0][2].innerHTML==='X') {
+        console.log("Az x játékos nyert.");
+    }
+    else if (TicArray[1][0].innerHTML === 'X' && TicArray[1][1].innerHTML==='X' && TicArray[1][2].innerHTML==='X') {
+        console.log("Az x játékos nyert.");
+    }
+    else if (TicArray[2][0].innerHTML === 'X' && TicArray[2][1].innerHTML==='X' && TicArray[2][2].innerHTML==='X') {
+        console.log("Az x játékos nyert.");
+    }
+    else if (TicArray[0][0].innerHTML === 'X' && TicArray[1][0].innerHTML==='X' && TicArray[2][0].innerHTML==='X') {
+        console.log("Az x játékos nyert.");
+    }
+    else if (TicArray[0][1].innerHTML === 'X' && TicArray[1][1].innerHTML==='X' && TicArray[2][1].innerHTML==='X') {
+        console.log("Az x játékos nyert.");
+    }
+    else if (TicArray[0][2].innerHTML === 'X' && TicArray[1][2].innerHTML==='X' && TicArray[2][2].innerHTML==='X') {
+        console.log("Az x játékos nyert.");
+    }
+    else if (TicArray[0][0].innerHTML === 'X' && TicArray[1][1].innerHTML==='X' && TicArray[2][2].innerHTML==='X') {
+        console.log("Az x játékos nyert.");
+    }
+    else if (TicArray[0][2].innerHTML === 'X' && TicArray[1][1].innerHTML==='X' && TicArray[2][0].innerHTML==='X') {
+        console.log("Az x játékos nyert.")
+    }
+    else if (TicArray[0][0].innerHTML === 'O' && TicArray[0][1].innerHTML==='O' && TicArray[0][2].innerHTML==='O') {
+        console.log("Az o játékos nyert.");
+    }
+    else if (TicArray[1][0].innerHTML === 'O' && TicArray[1][1].innerHTML==='O' && TicArray[1][2].innerHTML==='O') {
+        console.log("Az o játékos nyert.");
+    }
+    else if (TicArray[2][0].innerHTML === 'O' && TicArray[2][1].innerHTML==='O' && TicArray[2][2].innerHTML==='O') {
+        console.log("Az o játékos nyert.");
+    }
+    else if (TicArray[0][0].innerHTML === 'O' && TicArray[1][0].innerHTML==='O' && TicArray[2][0].innerHTML==='O') {
+        console.log("Az o játékos nyert.");
+    }
+    else if (TicArray[0][1].innerHTML === 'O' && TicArray[1][1].innerHTML==='O' && TicArray[2][1].innerHTML==='O') {
+        console.log("Az o játékos nyert.");
+    }
+    else if (TicArray[0][2].innerHTML === 'O' && TicArray[1][2].innerHTML==='O' && TicArray[2][2].innerHTML==='O') {
+        console.log("Az o játékos nyert.");
+    }
+    else if (TicArray[0][0].innerHTML === 'O' && TicArray[1][1].innerHTML==='O' && TicArray[2][2].innerHTML==='O') {
+        console.log("Az o játékos nyert.");
+    }
+    else if (TicArray[0][2].innerHTML === 'O' && TicArray[1][1].innerHTML==='O' && TicArray[2][0].innerHTML==='O') {
+        console.log("Az o játékos nyert.");
+    }
+};
+
+function NewGame() {
+    for (let i = 0; i < TicArray.length; i++) {
+        for (let j = 0; j < TicArray[i].length; j++) {
+                    TicArray[i][j].innerHTML = ' ';
+              
+        }
     }
 }
-console.log(TicArray);
-
-let signal = 'O';
-(function clickHandler() {
-    if(signal=='O') {
-        signal = 'X';
-    }else {
-        signal ='O';
-    }
-    TicArray.onclick= ()=> {TicArray.dataset.value = signal};   
-})();
-
-(function Rule() {
-    if (TicArray[0][0] === 'X' && TicArray[0][1]==='X' && TicArray[0][2]==='X') {
-        console.log("Az x játékos nyert.")
-    }
-    else if (TicArray[1][0] === 'X' && TicArray[1][1]==='X' && TicArray[1][2]==='X') {
-        console.log("Az x játékos nyert.")
-    }
-    else if (TicArray[2][0] === 'X' && TicArray[2][1]==='X' && TicArray[2][2]==='X') {
-        console.log("Az x játékos nyert.")
-    }
-    else if (TicArray[0][0] === 'X' && TicArray[1][0]==='X' && TicArray[2][0]==='X') {
-        console.log("Az x játékos nyert.")
-    }
-    else if (TicArray[0][1] === 'X' && TicArray[1][1]==='X' && TicArray[2][1]==='X') {
-        console.log("Az x játékos nyert.")
-    }
-    else if (TicArray[0][2] === 'X' && TicArray[1][2]==='X' && TicArray[2][2]==='X') {
-        console.log("Az x játékos nyert.")
-    }
-    else if (TicArray[0][0] === 'X' && TicArray[1][1]==='X' && TicArray[2][2]==='X') {
-        console.log("Az x játékos nyert.")
-    }
-    else if (TicArray[0][2] === 'X' && TicArray[1][1]==='X' && TicArray[2][0]==='X') {
-        console.log("Az x játékos nyert.")
-    }
-    else if (TicArray[0][0] === 'O' && TicArray[0][1]==='O' && TicArray[0][2]==='O') {
-        console.log("Az o játékos nyert.")
-    }
-    else if (TicArray[1][0] === 'O' && TicArray[1][1]==='O' && TicArray[1][2]==='O') {
-        console.log("Az o játékos nyert.")
-    }
-    else if (TicArray[2][0] === 'O' && TicArray[2][1]==='O' && TicArray[2][2]==='O') {
-        console.log("Az o játékos nyert.")
-    }
-    else if (TicArray[0][0] === 'O' && TicArray[1][0]==='O' && TicArray[2][0]==='O') {
-        console.log("Az o játékos nyert.")
-    }
-    else if (TicArray[0][1] === 'O' && TicArray[1][1]==='O' && TicArray[2][1]==='O') {
-        console.log("Az o játékos nyert.")
-    }
-    else if (TicArray[0][2] === 'O' && TicArray[1][2]==='O' && TicArray[2][2]==='O') {
-        console.log("Az o játékos nyert.")
-    }
-    else if (TicArray[0][0] === 'O' && TicArray[1][1]==='O' && TicArray[2][2]==='O') {
-        console.log("Az o játékos nyert.")
-    }
-    else if (TicArray[0][2] === 'O' && TicArray[1][1]==='O' && TicArray[2][0]==='O') {
-        console.log("Az o játékos nyert.")
-    }
-})();
